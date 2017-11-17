745bb5476ebd6dee9a6dbf6afe254860285ee14e
Anton <anton@pool-100-0-0-79.bstnma.fios.verizon.net>
2017-11-17 15:46:23 +0200

    wip

--- lib/lokale.rb
+++ lib/lokale.rb
@@ -1,78 +1,12 @@
 require "lokale/version"
 require "lokale/colorize"
 require "lokale/util"
+require "lokale/model"
 require "set"
 
-module Lokale 
 
-  class String
-    def localization_file?
-      File.directory?(self) == false &&
-          (self =~ /\/Pods\//) == nil &&
-          (self =~ /\.bundle\//) == nil &&
-          (self =~ /\/(.{1,8})\.lproj\//)
-    end
-
-    def source_file?
-      (File.directory?(self) == false) && (self =~ /\/Pods\//).nil? && ((self =~ /\.(swift|h|m)$/) != nil)
-    end
-  end
-
-
-  class LString
-    attr_accessor :key, :str, :note, :target
-
-    def initialize(key, str, note, target)
-      @key = key; @str = str; @note = note; @target = target
-    end
-
-    def self.strings_from_file(file_path, lang)
-      regex = /(?:\/* (.+) *\/.*\n)?"(.+)" *= *"(.+)";/
-      File.read(file_path).scan(regex).map { |m| LString.new(m[1], m[2], m[0], lang) }
-    end
-  end
-
-  #
-
+module Lokale
   class LFile
-    attr_reader :path, :lang, :name, :type
-    def initialize(file_path)
-      @path = file_path
-
-      File.basename(file_path) =~ /^(.+)\.([^\.]*?)$/
-      @name = $1
-      @type = $2
-
-      file_path =~ /\/(.{1,8})\.lproj\//
-      @lang = $1
-    end
-
-    def self.try_to_read(file_path)
-      return nil unless file_path.localization_file?
-      LFile.new(file_path)
-    end
-
-    def inspect 
-      "<#{@lang}/#{full_name}>"
-    end
-
-    def full_name
-      "#{@name}.#{@type}"
-    end
-
-    def strings_file?
-      @type == "strings" || @type == "stringsdict"
-    end
-
-    def parsed
-      return @parsed unless @parsed.nil?
-      @parsed = case type
-        when "strings"      then LString.strings_from_file(@path, @lang)
-        when "stringsdict"  then []
-        else nil
-      end      
-    end
-
     def repeats
       return [] if @parsed.nil?
       uniq_keys = keys.to_set
@@ -88,35 +22,6 @@ module Lokale
     def keys 
       return nil if parsed.nil?
       parsed.map(&:key)
-    end
-  end
-
-  #
-
-  class Macro
-    attr_reader :regex, :found_strings, :name, :file_name
-
-    def initialize(name, regex, file_name)
-      @name = name
-      @regex = regex
-      @file_name = file_name
-      clear_calls
-    end 
-
-    def clear_calls
-      @found_strings = Hash.new { |h, k| h[k] = 0 }
-    end
-
-    def read_from(file)
-      file.scan(@regex) { |m| @found_strings[m] += 1 }
-    end
-
-    def uniq_count
-      @found_strings.size
-    end
-
-    def total_count
-      @found_strings.values.reduce(:+) || 0
     end
   end
 end
--- lib/lokale/lokalefile.rb
+++ lib/lokale/lokalefile.rb
@@ -1,13 +1,46 @@
 
+require "lokale/config"
+require "lokale/model"
 
 class Config 
-	def read_lokalefile(project_path)
+  def read_lokalefile(project_path)
+    lokalefile_path = File.join(project_path, ".lokale")
+    if File.file? lokalefile_path 
+      config_from_file(lokalefile_path)
+    else
+      default_config
+    end
+  end
 
-	end
+  def default_config
+
+  end
+
+  def config_from_file(file_path, content=nil)
+    content ||= File.read(file_path)
+    p content
+    instance_eval(content)
+  end
 end
 
-class LokaleFile
-	def self.read()
 
-	end
+class Config 
+
+  attr_reader :macros
+
+  def add_macro(name)
+    p name
+    macro = Lokale::Macro.new()
+    yield macro
+    @macros ||= []
+    @macros << macro
+  end
 end
+
+
+def test 
+  Config.get.read_lokalefile File.dirname(__FILE__)
+
+end
+
+test
